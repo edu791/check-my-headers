c3aaf65016b8006944e57f95f879dc5ffc06a718
edu791 <1775903+edu791@users.noreply.github.com>
2024-10-16 15:34:43 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const { isUrlValid, makeRequest } = require('./helpers')
 const { setHeaders, setStatus, getAll } = require('./report')
 const { headerValidation } = require('./headerValidation')
 
-const checkMyHeaders = async (url) => {
+const checkMyHeaders = async (url, headers) => {
   if (!isUrlValid(url)) throw new Error('Invalid URL format!')
-  const { headers, statusCode } = await makeRequest(url)
+  const { headers, statusCode } = await makeRequest(url, headers)
   setHeaders(headers)
   setStatus(statusCode)
   headerValidation(headers)
